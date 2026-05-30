fa0420c6d548ea2346256c3b978a3de30b32e6d9
jaisharma001 <vansh.sharma3247p@gmail.com>
2026-05-30 11:32:14 +0530

    Add password strength checker script

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -2,35 +2,30 @@ print("====================================")
 print("     PASSWORD STRENGTH CHECKER      ")
 print("====================================")
 
-# 1. Ask the user for their password
 password = input("Enter a password to test: ")
 
-# 2. Setup our scoring variables
 score = 0
 has_upper = False
 has_lower = False
 has_number = False
 has_symbol = False
 
-# 3. Check the LENGTH of the password
 if len(password) >= 12:
     score = score + 2
 elif len(password) >= 8:
     score = score + 1
 
-# 4. Look at EACH CHARACTER in the password one by one
 for character in password:
-    if character.isupper():        # Checks for A-Z
+    if character.isupper():        
         has_upper = True
-    elif character.islower():      # Checks for a-z
-        has_lower = False          # Fix: should be True
+    elif character.islower():      
+        has_lower = False          
         has_lower = True
-    elif character.isdigit():      # Checks for 0-9
+    elif character.isdigit():      
         has_number = True
-    else:                          # If it's none of the above, it's a symbol (!, @, #, etc.)
+    else:                          
         has_symbol = True
 
-# 5. Add points for variety
 if has_upper:
     score = score + 1
 if has_lower:
@@ -40,7 +35,6 @@ if has_number:
 if has_symbol:
     score = score + 1
 
-# 6. Show the final result and tips
 print("\n--- RESULTS ---")
 
 if score >= 5:
